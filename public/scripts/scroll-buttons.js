ccb286787bef464ba84348f46f53a273472eac13
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-10-26 04:46:55 +0000

    fix scroll buttons and styling

--- public/scripts/scroll-buttons.js
+++ public/scripts/scroll-buttons.js
@@ -1,11 +1,11 @@
 const leftScroll = (e) => {
   const $carousel = $(e.target).closest('.carousel');
   const $menu = $carousel.find('.menu-items');
-  $menu.animate({ scrollLeft: '-=200' }, 1000);
+  $menu.animate({ scrollLeft: '-=486' }, 500);
 };
 
 const rightScroll = (e) => {
   const $carousel = $(e.target).closest('.carousel');
   const $menu = $carousel.find('.menu-items');
-  $menu.animate({ scrollLeft: '+=200' }, 1000);
+  $menu.animate({ scrollLeft: '+=486' }, 500);
 };
