4a7c56e3e31eaeb645e770f338dd722dbac9eb7f
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-10-27 07:08:04 +0000

    style popout and implement close on click

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,6 +5,7 @@ $(() => {
   // events go here
   $('.scroll-left').on('click', leftScroll);
   $('.scroll-right').on('click', rightScroll);
+  $('.close').on('click', () => $('.popout-section').css("visibility", "hidden"));
   $('.checkout').on('submit', checkout);
 });
 // research nodemon docs
