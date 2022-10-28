3e0be3f076d0bff93c6d601f93c731f85898bb4c
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-10-28 22:38:11 +0000

    rollback dbhelpers to luke Thurs 27

--- db/queries/dbhelpers.js
+++ db/queries/dbhelpers.js
@@ -20,15 +20,15 @@ const getMenuIdByName = (selectedName) => {
   return db.query('SELECT menu_items.id FROM menu_items WHERE name = $1;', [selectedName])
     .then((data) => {
       return data.rows;
-    });
-};
+    })
+}
 
 const getcartIdByUserId = (userID) => {
   return db.query('SELECT carts.id FROM carts WHERE user_id = $1;', [userID])
     .then((data) => {
       return data.rows;
-    });
-};
+    })
+}
 
 //menu routes section
 const getAllMenuItems = () => {
@@ -39,22 +39,22 @@ const getAllMenuItems = () => {
     .then((allMenuItems) => {
       let items = {};
       let feature_items = [];
-      for (const allMenuItem of allMenuItems) {
-        if (allMenuItem.feature_item) {
+      for(const allMenuItem of allMenuItems) {
+        if(allMenuItem.feature_item) {
           feature_items.push(allMenuItem);
         }
       };
       items["FEATURE_ITEMS"] = feature_items;
-      for (const allMenuItem of allMenuItems) {
+      for(const allMenuItem of allMenuItems) {
         let curr_type = allMenuItem['type'];
-        if (items.hasOwnProperty(curr_type)) {
+        if(items.hasOwnProperty(curr_type)) {
           items[curr_type].push(allMenuItem);
         } else {
           items[curr_type] = [allMenuItem];
         }
       };
       return items;
-    });
+    })
 };
 
 const getMenuItems = (inputVar) => {
@@ -67,39 +67,45 @@ const getMenuItems = (inputVar) => {
 //cart routes section
 const getCartPrice = (insertInfo) => {
   return db.query('SELECT cart_id, SUM(menu_items.price * cart_items.quantity) AS total_price FROM carts JOIN cart_items on carts.id = cart_id JOIN menu_items ON menu_item_id = menu_items.id WHERE cart_id = $1 GROUP BY cart_id;',
-    [insertInfo['cart_id']]
+  [insertInfo['cart_id']]
   )
     .then((data) => {
       return data.rows;
-    });
-};
+    })
+}
 
 const addCartItems = (insertInfo) => {
-  db.query(`INSERT INTO cart_items
-  (cart_id, menu_item_id, quantity, note) VALUES ($1, $2, $3, $4);`,
-    insertInfo.menuItem)
-    .then(data => {
-      console.log('data',data);
-      if (Number(data) > 0) {
-        const updateValue = Number(data) + Number(insertInfo['quantity']);
-
+  db.query('SELECT quantity FROM cart_items WHERE cart_id = $1 AND menu_item_id = $2 AND ( note = $3 OR $3 IS NULL);', [insertInfo['cart_id'], insertInfo['menu_item_id'], insertInfo['note']])
+  .then((data) => {
+    if(Array.isArray(data.rows) && data.rows.length === 0) {
+      return null;
+    } else {
+      const verifyInfos = data.rows;
+      const verifyQuantity = verifyInfos[0]['quantity'];
+      return verifyQuantity;
       }
-      if (Number(dataTwo === null)) {
-        db.query('INSERT INTO cart_items (cart_id, menu_item_id, quantity, note) VALUES ($1, $2, $3, $4);',
-          [insertInfo['cart_id'], insertInfo['menu_item_id'], insertInfo['quantity'], insertInfo['note']]);
-      }
-    });
+    })
+  .then((dataTwo) => {
+    if (Number(dataTwo) > 0) {
+      const updateValue = Number(dataTwo) + Number(insertInfo['quantity']);
+        db.query('UPDATE cart_items SET quantity = $1 WHERE cart_id = $2 AND menu_item_id = $3 AND ( note = $4 OR $4 IS NULL);', [updateValue, insertInfo['cart_id'], insertInfo['menu_item_id'], insertInfo['note']])
+    }
+    if (Number(dataTwo === null)) {
+      db.query('INSERT INTO cart_items (cart_id, menu_item_id, quantity, note) VALUES ($1, $2, $3, $4);',
+      [insertInfo['cart_id'], insertInfo['menu_item_id'], insertInfo['quantity'], insertInfo['note']])
+    }
+  })
   let dataOutput = [];
   return getCartPrice(insertInfo)
     .then((data) => {
       dataOutput.push(data);
-      return getCarts(insertInfo);
+      return getCarts(insertInfo)
     })
     .then((dataTwo) => {
       dataOutput.push(dataTwo);
       return dataOutput;
-    });
-};
+    })
+}
 
 const getCarts = (insertInfo) => {
   let dataOutput = [];
@@ -112,7 +118,7 @@ const getCarts = (insertInfo) => {
     .then((dataTwo) => {
       dataOutput.push(dataTwo);
       return db.query('SELECT carts.*, cart_items.*, menu_items.* FROM carts JOIN cart_items ON carts.id = cart_id JOIN menu_items ON menu_items.id = menu_item_id WHERE user_id = $1 AND cart_id = $2;',
-        [insertInfo['user_id'], insertInfo['cart_id']]);
+      [insertInfo['user_id'], insertInfo['cart_id']])
     })
     .then((dataThree) => {
       dataOutput.push(dataThree);
@@ -120,49 +126,32 @@ const getCarts = (insertInfo) => {
     })
     .catch((err) => {
       console.log("err", err.message);
-    });
-};
-
-const getCartItems = (insertInfo) => {
-  return db.query(
-    `SELECT cart_items.*, users.id as user_id
-    FROM cart_items
-    JOIN carts ON carts.id = cart_id
-    JOIN users ON users.id = user_id
-    WHERE user_id = $1;
-    `,
-    [insertInfo])
-    .then((data) => {
-      return data.rows;
     })
-    .catch((err) => {
-      console.log("err", err.message);
-    });
 };
 
 const removeCartItems = (insertInfo) => {
   db.query('SELECT quantity FROM cart_items WHERE cart_id = $1 AND menu_item_id = $2 AND ( note = $3 OR $3 IS NULL);', [insertInfo['cart_id'], insertInfo['menu_item_id'], insertInfo['note']])
-    .then((data) => {
-      if (Array.isArray(data.rows) && data.rows.length === 0) {
-        return null;
-      } else {
-        const verifyInfos = data.rows;
-        const verifyQuantity = verifyInfos[0]['quantity'];
-        return verifyQuantity;
+  .then((data) => {
+    if(Array.isArray(data.rows) && data.rows.length === 0) {
+      return null;
+    } else {
+      const verifyInfos = data.rows;
+      const verifyQuantity = verifyInfos[0]['quantity'];
+      return verifyQuantity;
       }
-    })
-    .then((dataTwo) => {
-      if (Number(dataTwo) - Number(insertInfo['quantity']) > 0) {
-        const updateValue = Number(dataTwo) - Number(insertInfo['quantity']);
-        db.query('UPDATE cart_items SET quantity = $1 WHERE menu_item_id = $2 AND note IS NULL;', [updateValue, insertInfo['menu_item_id']]);
-      }
-      if (Number(dataTwo) - Number(insertInfo['quantity']) === 0) {
-        db.query('DELETE FROM cart_items WHERE menu_item_id = $1;', [insertInfo['menu_item_id']]);
-      }
-      if (Number(dataTwo) - Number(insertInfo['quantity']) < 0) {
-        console.log('error');
-      }
-    });
+  })
+  .then((dataTwo) => {
+    if ( Number(dataTwo) - Number(insertInfo['quantity']) > 0 ) {
+      const updateValue = Number(dataTwo) - Number(insertInfo['quantity']);
+      db.query('UPDATE cart_items SET quantity = $1 WHERE menu_item_id = $2 AND note IS NULL;', [updateValue, insertInfo['menu_item_id']])
+    }
+    if (Number(dataTwo) - Number(insertInfo['quantity']) === 0 ) {
+      db.query('DELETE FROM cart_items WHERE menu_item_id = $1;', [insertInfo['menu_item_id']])
+    }
+    if (Number(dataTwo) - Number(insertInfo['quantity']) < 0 ) {
+      console.log('error');
+    }
+  })
   return getCarts(insertInfo);
 };
 
@@ -172,71 +161,71 @@ const deleteCartItems = (insertInfo) => {
     'DELETE FROM cart_items WHERE menu_item_id = $1;',
     [insertInfo['menu_item_id']]
   )
-    .then(() => {
-      return getCartPrice(insertInfo);
-    })
-    .then((data) => {
-      dataOutput.push(data);
-      return getCarts(insertInfo);
-    })
-    .then((dataTwo) => {
-      dataOutput.push(dataTwo);
-      return dataOutput;
-    })
-    .catch((err) => {
-      console.log("err", err.message);
-    });
-};
+  .then(() => {
+  return getCartPrice(insertInfo)
+  })
+  .then((data) => {
+    dataOutput.push(data);
+    return getCarts(insertInfo)
+  })
+  .then((dataTwo) => {
+    dataOutput.push(dataTwo);
+    return dataOutput;
+  })
+  .catch((err) => {
+  console.log("err", err.message);
+})
+}
 
 //order routes section
 const placeOrder = (insertInfo) => {
   return db.query('SELECT cart_items.*, menu_items.* FROM cart_items JOIN menu_items ON menu_items.id = menu_item_id JOIN carts ON carts.id = cart_id WHERE user_id = $1;', [insertInfo])
     .then((data) => {
-      return data.rows;
-    });
+      return data.rows
+    })
 };
 
 const orderConfirmation = (insertInfo) => {
   let inputArr = [];
   let orderNum;
-  db.query('INSERT INTO orders(price, cart_id) SELECT price, carts.id FROM carts WHERE user.id = $1;', [insertInfo['user_id']]);
+  db.query('INSERT INTO orders(price, cart_id) SELECT price, carts.id FROM carts WHERE carts.id = $1;', [insertInfo['cart_id']])
   return db.query(
     'SELECT cart_items.*, orders.id FROM cart_items JOIN carts ON carts.id = cart_items.cart_id JOIN orders ON carts.id = orders.cart_id WHERE orders.cart_id = $1;',
     [insertInfo['cart_id']]
   )
-    .then((data) => {
-      return data.rows;
-    })
-    .then((dataTwos) => {
-      for (const dataTwo of dataTwos) {
-        const inputObj = {
-          'quantity': dataTwo['quantity'],
-          'menu_item_id': dataTwo['menu_item_id'],
-          'note': dataTwo['note'],
-          'orders.id': dataTwo['id']
-        };
-        inputArr.push(inputObj);
-      }
-      orderNum = inputArr[0];
-      return inputArr;
-    })
-    .then((dataThrees) => {
-      for (const dataThree of dataThrees) {
-        db.query('INSERT INTO order_items (quantity, menu_item_id, note, order_id) VALUES ($1, $2, $3, $4);',
-          [dataThree['quantity'], dataThree['menu_item_id'], dataThree['note'], dataThree['orders.id']]);
-      }
-      db.query(
-        'DELETE FROM cart_items WHERE cart_id = $1;',
-        [insertInfo['cart_id']]
-      );
-    })
-    .then(() => {
-      return db.query('SELECT orders.*, order_items.* FROM orders JOIN order_items ON order_id = orders.id WHERE orders.id = $1', [orderNum['orders.id']]);
-    })
-    .then((dataFour) => {
-      return dataFour.rows;
-    });
-};
+  .then((data) => {
+    return data.rows;
+  })
+  .then((dataTwos) => {
+    for (const dataTwo of dataTwos) {
+      const inputObj = {
+        'quantity': dataTwo['quantity'],
+        'menu_item_id': dataTwo['menu_item_id'],
+        'note': dataTwo['note'],
+        'orders.id': dataTwo['id']
+      };
+      inputArr.push(inputObj);
+    }
+    orderNum = inputArr[0];
+    return inputArr;
+  })
+  .then((dataThrees) => {
+    for (const dataThree of dataThrees) {
+      db.query('INSERT INTO order_items (quantity, menu_item_id, note, order_id) VALUES ($1, $2, $3, $4);',
+      [dataThree['quantity'], dataThree['menu_item_id'], dataThree['note'], dataThree['orders.id']])
+    }
+    db.query(
+      'DELETE FROM carts WHERE carts.id = $1;',
+      [insertInfo['cart_id']]
+    )
+  })
+  .then(() => {
+    return db.query('SELECT orders.*, order_items.* FROM orders JOIN order_items ON order_id = orders.id WHERE orders.id = $1', [orderNum['orders.id']])
+  })
+  .then((dataFour) => {
+    return dataFour.rows;
+  })
+}
 
 module.exports = {
   getUsers,
@@ -244,7 +233,6 @@ module.exports = {
   getAllMenuItems,
   getMenuItems,
   getCarts,
-  getCartItems,
   addCartItems,
   getCartPrice,
   removeCartItems,
