750c73375f7738738d99e4037f9efcc7d27c8ccb
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-10-28 01:29:29 +0000

    modify scripst to fix bugs

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,14 +4,19 @@
 $(() => {
   const $popout = $('.popout-section');
   const $carousel = $('.dynamic-menu');
+  const $checkout = $('.order-section');
   console.log("alert test");
+
   // events go here
   $carousel.on('click', '.scroll-left', leftScroll);
   $carousel.on('click', '.scroll-right', rightScroll);
   $carousel.on('click', '.expand-food', renderPopout);
+  $carousel.on('click', '.expand-food', addToCart);
   $popout.on('click', '.toggle-less', decreaseQuantity);
   $popout.on('click', '.toggle-more', increaseQuantity);
   $popout.on('click', '.close', () => $('.popout-section').css("visibility", "hidden"));
+  $checkout.on("click", '.remove-item', deleteItem);
+
   $('.checkout').on('submit', checkout);
 });
 // research nodemon docs
