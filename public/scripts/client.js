6254b22b484c63fe5b14209fa8f346ebe015da6d
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-10-28 22:19:32 +0000

    convert to single page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,22 +2,24 @@
 
 // Document ready
 $(() => {
+  const $body = $('body');
   const $popout = $('.popout-section');
   const $carousel = $('.dynamic-menu');
   const $checkout = $('.order-section');
-  const $header = $('header');
-  console.log("alert client");
-
+  const $display1 = $('#display1');
+  const $display2 = $('#display2');
   // events go here
-  $header.on('click', '.back-to-homepage', backToRestaurant);
   $carousel.on('click', '.scroll-left', leftScroll);
   $carousel.on('click', '.scroll-right', rightScroll);
   $carousel.on('click', '.expand-food', renderPopout);
   $popout.on('click', '.toggle-less', decreaseQuantity);
   $popout.on('click', '.toggle-more', increaseQuantity);
   $popout.on('click', '.add-to-order-button', addToCart);
+
+  // display change events
   $popout.on('click', '.close', () => $popout.css("visibility", "hidden"));
-  // $checkout.on("click", '.remove-item', deleteItem);
-  $checkout.on('submit', checkout);
+  $display1.on('click', '.checkout', goToCheckout);
+  $display2.on('click', '.back-to-homepage', goToHome);
+
 });
 // research nodemon docs
