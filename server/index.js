266bab807f92e575f03369cfd9dc31f2efdd19d9
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-10-29 01:48:55 +0000

    adjust checkout page

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   '/styles',
