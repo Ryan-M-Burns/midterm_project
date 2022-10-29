ae2f3fbadbc53be43e4753759385060f18677125
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-10-29 03:15:57 +0000

    update dynamic creation
    update to restore consistency to match my previously written styling and code for the cart

--- public/scripts/orderCheckOut.js
+++ public/scripts/orderCheckOut.js
@@ -24,25 +24,9 @@ $(document).ready(function(){
   $(".order-summary").append(cartSection);
   });
 
-  // $(".food-picture").on("click", function(){
-  //   const searchKey = $(this).parent().siblings(".menu-item-text").children(".food-name");
-  //   const searchKeyValue = searchKey.text();
-    // function callback(val) {
-    //   $(".order-summary").empty();
-    //   // console.log('val', val['varInput'][0][0]['total_price'])
-    //   // console.log('val', val['varInput'][0][0]['cart_id'])
-    //   // console.log('val', val['varInput'][1][0])
-    //   // console.log('val', val['varInput'][1][1].rows)
-    //   const infoInputs = val['varInput'][1][1].rows;
-    //   let cartSection = ``;
-    //   cartSection += orderListItems(infoInputs);
-    //   $(".order-summary").append(cartSection);
-    // };
-  //   $.post('/cart', searchKeyValue, callback, "json");
-  // })
   $(document).on("click", '.remove-item', function(){
-    const deleteMenuName = $(this).parent().children(".food-name-incart").text();
-    const deleteItemQuantity = $(this).parent().children(".food-quantity-incart").text();
+    const deleteMenuName = $(this).parent().children(".food-name").text();
+    const deleteItemQuantity = $(this).parent().children(".food-quantity").text();
     const infoInsert = {};
     infoInsert['name'] = deleteMenuName;
     infoInsert['quantity'] = deleteItemQuantity;
@@ -57,18 +41,26 @@ $(document).ready(function(){
   })
 })
 
+
+// {/* <div class="food-quantity"><span>${infoInput.quantity}</span><span>x</span></div> */}
+
 //helper function
 const orderListItems = (infoInputs) => {
   let cart_section = ``;
   for (const infoInput of infoInputs) {
     const cartItem = `
-    <div class="order-items-incart">
-      <div class="food-quantity-incart">${infoInput.quantity}</div>
-      <div class="food-name-incart" id=${infoInput.menu_item_id}>${infoInput.name}</div>
-      <div class="foodpic-div-incart">
-        <img src="${infoInput.image_url}" alt="food" class="food-picture-incart">
+    <div class="order-items">
+    <div class="quantity-item">
+          <div>
+            <span>${infoInput.quantity}</span><span>x</span>
+        </div>
+          <p>${infoInput['name']}</p>
+        </div>
+      <div class="food-name" id=${infoInput.menu_item_id}>${infoInput.name}</div>
+      <div class="foodpic-div">
+        <img src="${infoInput.image_url}" alt="food" class="food-picture">
       </div>
-      <div class="food-price-incart">$${(infoInput.quantity * infoInput.price / 100).toFixed(2)}</div>
+      <div class="food-price">$${(infoInput.quantity * infoInput.price / 100).toFixed(2)}</div>
         <button type="button" class="remove-item">
         <i class="fa-solid fa-trash-can" class="deleteCartItems"></i>
         </button>
