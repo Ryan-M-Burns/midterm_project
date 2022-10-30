0c39b07029317b9841f088b82e22b9635d4f928d
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-10-30 07:35:36 +0000

    add functionality to tip buttons, fix styling issues

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,6 +10,16 @@ $(() => {
   const $display1 = $('#display1');
   const $display2 = $('#display2');
   const $header = $('header');
+  const $tipArea = $("#custom-tip-box");
+
+  // tip handlers
+  $tipArea.hide();
+  $('#tip-button1').on('click', addTip(0.1));
+  $('#tip-button2').on('click', addTip(0.15));
+  $('#tip-button3').on('click', addTip(0.2));
+  $('#tip-button4').on('click', toggleTipBox);
+  $('#custom-tip').on('keypress', customTip);
+
   // events go here
   $carousel.on('click', '.scroll-left', leftScroll);
   $carousel.on('click', '.scroll-right', rightScroll);
@@ -20,7 +30,6 @@ $(() => {
 
   //$checkout.on("reset", () => $("#tweet-text").val("").trigger("input"));
   // display change events
-  $('#tip-button4').on('click', toggleTipBox);
   $popout.on('click', '.close', () => $popout.css("visibility", "hidden"));
   $display1.on('click', '.checkout', goToCheckout);
   $header.on('click', '#BTR', goToHome);
--- public/scripts/tips.js
+++ public/scripts/tips.js
@@ -1,6 +1,23 @@
 const toggleTipBox = () => {
-  const $tipBox = $("#custom-tip");
-  $tipBox.toggle(400);
-  $tipBox.focus();
+  const $tipBox = $("#custom-tip-box");
+  $tipBox.toggle("fast", "linear");
 };
+
+const customTip = (e) => {
+
+  if (e.keyCode == 13) {
+    e.preventDefault();
+    const tip = parseFloat($('#custom-tip').val());
+    return addTip(tip/100);
+  }
+
+};
+const addTip = (tip) => {
+  const subtotal = $('#order-subtotal').text().replace('$', '');
+  const tipAmount = parseFloat(subtotal) * tip;
+  toggleTipBox();
+  $('#order-tip').empty();
+  $('#order-tip').append(`$${tipAmount.toFixed(2)}`);
+}
+
 
